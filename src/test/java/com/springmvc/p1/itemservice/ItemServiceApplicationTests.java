749de49c08b7558646e8f124ca9ec5ba9b8206bf
heejunByun <hejeun22@gmail.com>
2022-09-11 22:59:47 +0900

    item-service

---
+++ src/test/java/com/springmvc/p1/itemservice/ItemServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springmvc.p1.itemservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
