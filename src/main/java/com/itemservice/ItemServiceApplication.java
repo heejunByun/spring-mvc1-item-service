50978485de3f65971469c10995e8c27138eda5f4
heejunByun <hejeun22@gmail.com>
2022-09-11 23:05:44 +0900

    pkg refresh

--- src/main/java/com/itemservice/ItemServiceApplication.java
+++ src/main/java/com/itemservice/ItemServiceApplication.java
@@ -1,4 +1,4 @@
-package com.springmvc.p1.itemservice;
+package com.itemservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
